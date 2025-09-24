2a3df3e7e5a66ac856efc2fb2f3574ade61582f5
Leeee Lehtomaki <153555185+tornadolehto@users.noreply.github.com>
2025-09-24 16:08:39 -0400

    updated 2022 (pray this works)
    changed some stuff around further changes probably necessary

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,20 +9,21 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
-  private CommandXboxController m_controller;
+  private XboxController m_controller;
 
   private final PWMVictorSPX m_leftMotor = new PWMVictorSPX(4);
   private final PWMVictorSPX m_rightMotor = new PWMVictorSPX(5);
 
-  private final Servo eyebrowLeft  = new Servo(0); 
-  private final Servo stopArm      = new Servo(1); 
-  private final Servo eyebrowRight = new Servo(2); 
-  private final Servo mysteryThing = new Servo(3); 
+  private final Servo eyebrowLeft = new Servo(0);
+  private final Servo stopArm = new Servo(1);
+  private final Servo eyebrowRight = new Servo(2);
+  private final Servo mysteryThing = new Servo(3);
 
   private final PWMVictorSPX yellowLight = new PWMVictorSPX(6);
   private final PWMVictorSPX redLight = new PWMVictorSPX(7);
@@ -34,50 +35,58 @@ public class Robot extends TimedRobot {
   private boolean eyebrowRightUp = false;
   private boolean stopArmOut = false;
   private boolean mysteryExtended = false;
+  private Trigger aButtonTrigger = new Trigger(m_controller::getAButton);
+  private Trigger bButtonTrigger = new Trigger(m_controller::getBButton);
+  private Trigger xButtonTrigger = new Trigger(m_controller::getXButton);
+  private Trigger leftBumperTrigger = new Trigger(m_controller::getLeftBumper);
 
   @Override
   public void robotInit() {
     m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
     m_rightMotor.setInverted(true);
 
-    m_controller = new CommandXboxController(0);
+    m_controller = new XboxController(0);
 
     // Lights toggle on A
-    m_controller.a().onTrue(Commands.runOnce(() -> flashYellow = true))
-                   .onFalse(Commands.runOnce(() -> flashYellow = false));
+    aButtonTrigger.whenActive(new InstantCommand(() -> flashYellow = true))
+        .whenInactive(new InstantCommand(() -> flashYellow = false));
 
     // Servo button bindings
-    m_controller.x().onTrue(Commands.runOnce(() -> toggleServo(eyebrowLeft, () -> eyebrowLeftUp, v -> eyebrowLeftUp = v)));
-    m_controller.y().onTrue(Commands.runOnce(() -> toggleServo(eyebrowRight, () -> eyebrowRightUp, v -> eyebrowRightUp = v)));
-    m_controller.b().onTrue(Commands.runOnce(() -> toggleServo(stopArm, () -> stopArmOut, v -> stopArmOut = v)));
-    m_controller.leftBumper().onTrue(Commands.runOnce(() -> toggleServo(mysteryThing, () -> mysteryExtended, v -> mysteryExtended = v)));
+    aButtonTrigger
+        .whenActive(new InstantCommand(() -> toggleServo(eyebrowLeft, () -> eyebrowLeftUp, v -> eyebrowLeftUp = v)));
+    bButtonTrigger
+        .whenActive(new InstantCommand(() -> toggleServo(eyebrowRight, () -> eyebrowRightUp, v -> eyebrowRightUp = v)));
+    xButtonTrigger.whenActive(new InstantCommand(() -> toggleServo(stopArm, () -> stopArmOut, v -> stopArmOut = v)));
+    leftBumperTrigger.whenActive(
+        new InstantCommand(() -> toggleServo(mysteryThing, () -> mysteryExtended, v -> mysteryExtended = v)));
 
     flashTimer.start();
   }
 
   @Override
   public void teleopPeriodic() {
+    if (!flashTimer.advanceIfElapsed(0.5)) {
+      return;
+    }
+
     // Flashing lights
     if (flashYellow) {
       redLight.set(0);
-      if (flashTimer.advanceIfElapsed(0.5)) {
-        double current = yellowLight.get();
-        if (Math.abs(current) > 0.5) {
-          yellowLight.set(0.0);
-        } else {
-          yellowLight.set(1.0);
-        }
+      double current = yellowLight.get();
+      if (Math.abs(current) > 0.5) {
+        yellowLight.set(0.0);
+      } else {
+        yellowLight.set(1.0);
       }
     } else {
       yellowLight.set(0);
-      if (flashTimer.advanceIfElapsed(0.5)) {
-        double current = redLight.get();
-        if (Math.abs(current) > 0.5) {
-          redLight.set(0.0);
-        } else {
-          redLight.set(1.0);
-        }
+      double current = redLight.get();
+      if (Math.abs(current) > 0.5) {
+        redLight.set(0.0);
+      } else {
+        redLight.set(1.0);
       }
+
     }
 
     // Tank drive
@@ -86,7 +95,8 @@ public class Robot extends TimedRobot {
     m_myRobot.tankDrive(leftPower, rightPower);
   }
 
-  private void toggleServo(Servo servo, java.util.function.Supplier<Boolean> getter, java.util.function.Consumer<Boolean> setter) {
+  private void toggleServo(Servo servo, java.util.function.Supplier<Boolean> getter,
+      java.util.function.Consumer<Boolean> setter) {
     boolean state = !getter.get();
     setter.accept(state);
     servo.set(state ? 1.0 : 0.0); // 1.0 = extended, 0.0 = retracted
